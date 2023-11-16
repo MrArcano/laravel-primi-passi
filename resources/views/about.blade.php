ff8b1d0422704275d7a42211babbcd6e71537686
MrArcano <salvatore-dario@hotmail.it>
2023-11-16 16:00:00 +0100

    [edit]: head

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,17 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel</title>
-</head>
-<body>
-    <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-    </ul>
+@include('head')
 
-    <h1>About - Laravel</h1>
+<body>
+    @include('nav')
+    <div class="container my-5">
+        <h1>About - Laravel</h1>
+    </div>
 </body>
 </html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel</title>
-</head>
-<body>
-    <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-    </ul>
+@include('head')
 
-    <h1>Home - Laravel</h1>
-    <p> {{ $text }} </p>
+<body>
+    @include('nav')
+    <div class="container my-5">
+        <h1>Home - Laravel</h1>
+        <p> {{ $text }} </p>
+    </div>
+
 </body>
 </html>
